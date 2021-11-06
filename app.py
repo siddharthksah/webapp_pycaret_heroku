7d911b11c5064cf77d4be44c0d78290f87c50079
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-11-06 23:02:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import numpy as np
 
 app = Flask(__name__)
 
-model = load_model('deployment_28042020')
+model = load_model('model')
 cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("templates/home.html")
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -20,7 +20,7 @@ def predict():
     data_unseen = pd.DataFrame([final], columns = cols)
     prediction = predict_model(model, data=data_unseen, round = 0)
     prediction = int(prediction.Label[0])
-    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
+    return render_template('templates/home.html',pred='Expected Bill will be {}'.format(prediction))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
